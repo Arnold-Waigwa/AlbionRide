9cf92cf28bb014234c6a61828a628af1e68fe5c4
Arnold Waigwa <waigwaarnold@gmail.com>
2025-03-10 13:41:23 -0400

    added homepage markup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { getImageUrl } from "../utils";
+import { Link } from "react-router-dom";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.navbarImage}>
+        <img src={getImageUrl("nav/Albionride.jpeg")} alt="Albion Ride Logo" />
+      </div>
+      <div className={styles.navbarContainer}>
+        <h3 className={styles.title}>Albion Ride</h3>
+        <ul className={styles.menuItems}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Log in/Sign up</Link>
+          </li>
+          <li>
+            <Link to="/findaride">Find a Ride</Link>
+          </li>
+          <li>
+            <Link to="/offeraride">Offer a Ride</Link>
+          </li>
+          <li
+            className={styles.dropdown}
+            onMouseEnter={() => setIsDropdownOpen(true)}
+            onMouseLeave={() => setIsDropdownOpen(false)}
+          >
+            <span className={styles.moreBtn}>More ▼</span>
+            {isDropdownOpen && (
+              <ul className={styles.dropdownMenu}>
+                <li>
+                  <Link to="/safetyandguidelines">Safety & Guidelines</Link>
+                </li>
+                <li>
+                  <Link to="/termsandservices">Terms & Services</Link>
+                </li>
+                <li>
+                  <Link to="/contactus">Contact Us</Link>
+                </li>
+              </ul>
+            )}
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
---
+++ src/utils.js
@@ -0,0 +1,2 @@
+//create a utility function that has absolute path and only requires relative path
+export const getImageUrl = (path) => `/assets/${path}`;
