b5182d259f1e4852d33d6351c68010273c667ee8
Arnold Waigwa <waigwaarnold@gmail.com>
2025-03-12 14:08:07 -0400

    added hero section

---
+++ src/components/homepagecomponents/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Hero.module.css";
+
+const Hero = () => {
+  return (
+    <div className={styles.hero}>
+      <h1>Welcome to Albion Ride</h1>
+      <h2>Helping Albion Students Travel with Ease</h2>
+    </div>
+  );
+};
+
+export default Hero;
