6d971e467d56e0ed015c3f534cc959a9c97749f7
Arnold Waigwa <agw13@albion.edu>
2025-04-02 17:47:11 -0400

    started Find a ride page

--- src/components/findaridecomponents/Hero.jsx
+++ src/components/findaridecomponents/Hero.jsx
@@ -1,10 +1,26 @@
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
-    <div>
-      <img src={getImageUrl("Images/arrow.png")} alt="Image of arrow" />
+    <div className={styles.heroContainer}>
+      <img
+        src={getImageUrl("Images/arrow.png")}
+        alt="Image of arrow"
+        className={styles.arrow}
+      />
+      <img
+        src={getImageUrl("Images/roadtrip.png")}
+        alt="image of road trip"
+        className={styles.roadTrip}
+      />
+      <Link to="/" className={styles.button}>
+        Find a Ride
+      </Link>
+      <Link to="/" className={styles.button}>
+        Publish a Ride
+      </Link>
     </div>
   );
 };
